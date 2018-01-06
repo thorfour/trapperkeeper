f0430865acee9a7cdf6568ad424d5dca26f24e40
Thor <thor.hansen@coreos.com>
2018-01-05 20:49:50 -0800

    added build dir

---
+++ build/index.js
@@ -0,0 +1,49 @@
+var DEBUG = false
+var cp = require("child_process");
+var request = require('request')
+var headers = {
+    'User-Agent': 'Super Agent/0.0.1',
+    'Content-Type': 'application/json',
+}
+
+exports.handler = function(req, res) {
+
+    var queryStr = JSON.stringify(req.body);
+
+    if (DEBUG) {
+        console.log(queryStr);
+    }
+
+    var proc = cp.spawnSync("./cmd", [queryStr], {stdio: 'pipe', encoding: "utf8"});
+    var quote = proc.stdout;
+
+    var respType = "in_channel";
+    // Check for no response, means there was an error
+    if (quote === "") {
+        quote = proc.stderr
+        respType = "ephemeral";
+
+        if (DEBUG) {
+            console.log(proc.stderr);
+        }
+    }
+
+    // Parse quote into json for slack
+    var resp = '{ "response_type" : "' + respType + '", "text" : "' + quote + '" }';
+
+    var options = {
+        url: req.body.response_url,
+        method: 'POST',
+        headers: headers,
+        form: resp
+    }
+
+    if (DEBUG) {
+        console.log(req.body.response_url);
+    }
+
+    // Return json
+    request(options);
+
+    res.status(200).end();
+};
--- cmd/main.go
+++ cmd/main.go
@@ -10,14 +10,21 @@ import (
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprint(os.Stderr, "expected arg")
+		return
+	}
+
 	m := make(map[string]string)
 	if err := json.Unmarshal([]byte(os.Args[1]), &m); err != nil {
 		fmt.Fprint(os.Stderr, "json.Unmarshal: ", err)
+		return
 	}
 
 	text := strings.Split(m["text"], " ")
 
 	if err := keeper.Handle(text[0], m["user_id"], m["user_name"], text[1:]); err != nil {
 		fmt.Fprint(os.Stderr, err.Error())
+		return
 	}
 }
