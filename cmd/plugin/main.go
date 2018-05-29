dee6b78c7bbd2a6b502e5449733a3394ea811561
Thor <thansen@digitalocean.com>
2018-05-29 15:33:49 -0500

    Added env redis handling to plugin for external connections

--- cmd/plugin/main.go
+++ cmd/plugin/main.go
@@ -2,27 +2,27 @@ package main
 
 import (
 	"net/url"
+	"os"
 	"strings"
 
 	"github.com/alicebob/miniredis"
 	"github.com/thorfour/trapperkeeper/pkg/keeper"
 )
 
-var (
-	setupErr error
-)
-
 func init() {
-	var rds *miniredis.Miniredis
-	rds, setupErr = miniredis.Run()
-	keeper.RedisAddr = rds.Addr()
+	keeper.RedisAddr = os.Getenv("REDISADDR")
+	keeper.RedisPw = os.Getenv("REDISPW")
+
+	// Use local redis if external one does not exist
+	if keeper.RedisAddr == "" {
+		rds, _ := miniredis.Run()
+		keeper.RedisAddr = rds.Addr()
+		keeper.RedisPw = ""
+	}
 }
 
 // Handler is the plugin handler
 func Handler(v url.Values) (string, error) {
-	if setupErr != nil {
-		return "", setupErr
-	}
 	text := strings.Split(v["text"][0], " ")
 	return keeper.Handle(text[0], v["team_id"][0], v["user_id"][0], v["user_name"][0], text[1:])
 }
